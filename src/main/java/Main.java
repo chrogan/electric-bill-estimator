e2b359b64d56c58c00fc778f123fdee9cd8d4062
DTNevaeha <DTNevaeha@gmail.com>
2022-08-08 15:50:30 -0500

    Started JavaDocs, Created state costs file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,14 @@
 public class Main {
 
+  private Main() {
+  }
+
+  /**
+   *Creates a class for users to calculate estimate electric costs using default or input values.
+   */
+  public static void main(String[] args) {
+  }
+
+
+
 }
