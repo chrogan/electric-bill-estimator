75772d71d5eb7ce37a40770148a2fabd0b099054
DTNevaeha <DTNevaeha@gmail.com>
2022-08-08 15:58:06 -0500

    Updated Javadocs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,10 @@ public class Main {
 
   /**
    *Creates a class for users to calculate estimate electric costs using default or input values.
+   *This calculator uses either the default pre-set values for multiple appliances or user input.
+   *If the user inputs invalid values then an instance of {@link java.io.IOException} is thrown.
    */
+
   public static void main(String[] args) {
   }
 
