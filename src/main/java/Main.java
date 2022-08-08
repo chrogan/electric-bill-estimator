6ba72da54a49ae0986c1b5d092d58de0c6b2450b
akwarkoh <angelakwarko@gmail.com>
2022-08-08 14:46:14 -0700

    created constructors and set their values

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,15 @@
 public class Main {
 
+  public static void main(String[] args) {
+    Appliance AC = new Appliance(120.0, 37.5, 17.5);// voltage= 120, Amps= 37.5, hours=17.5
+    Appliance heater = new Appliance(120.0, 12.5,12.0);//voltage=120, Amps = 12.5, hours=12
+    Appliance tv = new Appliance(120.0, 0.5, 7.0 );//voltage= 120, Amps=0.5 , hours= 7
+    Appliance refrigerator = new Appliance(110.0, 17.5, 8.0);//voltage=110, Amps=17.5, hours=8
+    Appliance dishwasher = new Appliance(110.0,15.0,3.0);//voltage=110 Amps=15, hours =3
+    Appliance microwave = new Appliance(240, 20.0,0.5);//voltage=240, Amps= 20, hours=0.5
+    Appliance lighting= new Appliance();//voltage=
+
+
+  }
+
 }
