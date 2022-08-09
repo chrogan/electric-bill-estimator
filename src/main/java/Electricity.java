7c6ca9b2f7d41336f3c5e84de8198f9a019c8b40
chrogan <chrogan@amazon.com>
2022-08-09 09:24:07 -0700

    Seperated bill and electricity

--- src/main/java/Electricity.java
+++ src/main/java/Electricity.java
@@ -1,67 +1,47 @@
-public class Electricity {
+import java.util.ArrayList;
+import java.util.List;
 
-//COST = KWH
-//  KWH = (KW * 30)
-//      KW = (WATT / 1000)
-//        WATT = (AMP * VOLT)
+public class Electricity {
 
   //Fields
 
   private static int instanceCount = 0;
 
-  public double deviceKWH;
   public double volts;
   public double amps;
   public double hours;
+  public static double totalPower;
 
+public static List<Appliance> devices = new ArrayList<>();
 
   //Constructors
   public Electricity() {
+
   }
   public Electricity(Appliance appliance) {
     this.volts = appliance.getVoltage();
     this.amps = appliance.getAmps();
     this.hours = appliance.getHours();
+    devices.add(appliance);
   }
 
 
   //METHODS
 
-  //TODO create power calculator (per devices)
+  //TODO create totalPower
+  private double totalPowerCalculator() {
+    //TODO sum of getPowerCalculator for each appliance
 
-  //returns each device's power in kW
-  private double devicePowerCalculator(double voltage, double current) {
-    Electricity electricity = new Electricity();
-    return electricity.powerPerDevice = current * voltage / 1000.0;
-  }
-
-  private double deviceKWHCalculator(double hours) {
-    return electricity.deviceKWH = getPowerPerDevice() * hours;
-  }
-
-
-
-  //accessors and modifiers
-
-  public static double getTotalMonthlyBill() {
-    return totalMonthlyBill;
-  }
-
-  public static double getTotalYearlyBill() {
-    return totalYearlyBill;
-  }
-
-  public static double getPowerPerDevice() {
-    return powerPerDevice;
-  }
-
-
-  public static double getTotalPower() {
+    for(Appliance device : devices){
+      Electricity electricity = new Electricity(device);
+       totalPower += electricity.getDeviceKWH();
+    }
     return totalPower;
   }
 
-
-  public static double getDeviceKWH() {
+  // Returns in kWh/power per hours
+  private double deviceKWH() {
+    double deviceKWH = devicePower() * hours;
     return deviceKWH;
   }
 
@@ -71,22 +51,6 @@ public class Electricity {
     return result;
   }
 
-  private double devicePower() {
-    double result = (volts * amps)/ 1000.0;
-    return result;
-  }
-
-  //ACCESSORS AND MODIFIERS
-
-  public double getTotalPower() {
-    return totalPowerCalculator();
-  }
-  public double getDeviceKWH() {
-    return deviceKWHCalculator();
-  }
-  public double getPowerPerDevice() {
-    return devicePower();
-  }
 
   //ACCESSORS AND MODIFIERS
 
