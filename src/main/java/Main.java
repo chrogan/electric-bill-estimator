2fdad7d09002ceaa65478289688d6806594c7185
chrogan <chrogan@amazon.com>
2022-08-09 06:38:06 -0700

    Electricity is starting to read values.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,6 +14,13 @@ public class Main {
     Appliance microwave = new Appliance(240, 20.0,0.5);//voltage=240, Amps= 20, hours=0.5
     Appliance lighting= new Appliance();//voltage=
 
+    Electricity electricity = new Electricity(tv);
+
+
+    double power = electricity.getPowerPerDevice();
+    double Kwh = electricity.getDeviceKWH();
+    System.out.println(power);
+    System.out.println(Kwh);
 
   }
 
