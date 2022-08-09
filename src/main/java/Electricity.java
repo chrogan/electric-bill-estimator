6f94661748fc0025e460762d8433901e2557bdf4
chrogan <chrogan@amazon.com>
2022-08-09 09:24:07 -0700

    Seperated bill and electricity

--- src/main/java/Electricity.java
+++ src/main/java/Electricity.java
@@ -1,4 +1,5 @@
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class Electricity {
 
@@ -6,21 +7,22 @@ public class Electricity {
 
   private static int instanceCount = 0;
 
-  public double deviceKWH;
   public double volts;
   public double amps;
   public double hours;
+  public static double totalPower;
 
+public static List<Appliance> devices = new ArrayList<>();
 
   //Constructors
   public Electricity() {
-    instanceCount++;
 
   }
   public Electricity(Appliance appliance) {
     this.volts = appliance.getVoltage();
     this.amps = appliance.getAmps();
     this.hours = appliance.getHours();
+    devices.add(appliance);
   }
 
 
@@ -28,20 +30,27 @@ public class Electricity {
 
   //TODO create totalPower
   private double totalPowerCalculator() {
-    //TODO getPowerCalculator for each device and multiply by number of devices
-    double totalPower = 0;
+    //TODO sum of getPowerCalculator for each appliance
 
+    for(Appliance device : devices){
+      Electricity electricity = new Electricity(device);
+       totalPower += electricity.getDeviceKWH();
+    }
     return totalPower;
   }
-  private double deviceKWHCalculator() {
-    deviceKWH = devicePower() * hours;
+
+  // Returns in kWh/power per hours
+  private double deviceKWH() {
+    double deviceKWH = devicePower() * hours;
     return deviceKWH;
   }
 
+  //Returns in kW/power
   private double devicePower() {
     double result = (volts * amps)/ 1000.0;
     return result;
   }
+
 
   //ACCESSORS AND MODIFIERS
 
@@ -49,10 +58,9 @@ public class Electricity {
     return totalPowerCalculator();
   }
   public double getDeviceKWH() {
-    return deviceKWHCalculator();
+    return deviceKWH();
   }
-  public double getPowerPerDevice() {
+  public double getDevicePower() {
     return devicePower();
   }
-
 }
