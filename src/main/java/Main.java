17235c9e38106c92ce9e8c334560897fad38a73b
chrogan <chrogan@amazon.com>
2022-08-11 11:13:30 -0700

    Code Complete, maybe polish

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,9 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 import java.util.TreeMap;
 import java.util.stream.Collectors;
 
@@ -17,7 +17,7 @@ public class Main {
    * If the user inputs invalid values then an instance of {@link java.io.IOException} is thrown.
    */
   public static void main(String[] args) {
-
+    //Gets the rate from a specific state after user input
     Scanner scanner = new Scanner(System.in);
     System.out.printf("Please provide the state abbreviation of your residency:\n"
         + "(example: Enter 'NY' for New York or 'CO' for Colorado)\n ");
@@ -27,7 +27,7 @@ public class Main {
           .lines(Paths.get("src/main/resources/state-costs.csv"))
           .map((line) -> line.trim().split("\\s*,\\s*"))
           .collect(Collectors.toMap((parts) -> parts[0], (parts) -> Double.valueOf(parts[1])));
-      System.out.println(states.get(state));
+      System.out.println("The average electricity cost in " + state + " = $" + states.get(state));
       rate = states.get(state);
 
     } catch (IOException e) {
@@ -37,67 +37,50 @@ public class Main {
     System.out.printf("How many appliances would you like to register?\n");
     int numAppliances = scanner.nextInt();
 
-    Map<String, Appliance> appliances = new TreeMap<>();//Linked HashMap would iterate based on input order
-    //TreeMap<>(Comparator.comparing(String::compareToCaseInsensitive));
-    for (int i = 0; i< numAppliances; i++) {
+    Map<String, Appliance> appliances = new TreeMap<>();
+
+    Set<String> names = appliances.keySet();
+    String name;
+    for (int i = 0; i < numAppliances; i++) {
       scanner.nextLine();
-      //Create while loop
       System.out.printf("Which appliance would you like to register?\n" +
           "examples: Television, Air Conditioning, Dishwasher, Microwave\n");
-      String name = scanner.nextLine();
+      name = scanner.nextLine();
+      if (appliances.containsKey(name)) {
+        System.out.println("Sorry you already gave that name. Please insert a new device.");
+      }
+      while (!appliances.containsKey(name)) {
+        System.out.printf("What is the voltage required for this appliance?\n"
+            + "(Note: Most appliances in the United States have a voltage from"
+            + " 110-120 volts or 220 -240 volts.\n");
+        double volts = scanner.nextInt();
 
-      System.out.printf("What is the voltage required for this appliance?\n +"
-          + "(Note: Most appliances in the United States have a voltage from"
-              + "110-120 volts or 220 -240 volts.\n");
-      double volts = scanner.nextInt();
+        System.out.printf("What is the amperage required for this appliance?\n");
+        double amps = scanner.nextDouble();
 
-      System.out.printf("What is the amperage required for this appliance?\n");
-      double amps = scanner.nextDouble();
+        System.out.printf("How many hours a day do you usually run this appliance?\n");
+        double hours = scanner.nextDouble();
 
-      System.out.printf("How many hours a day do you usually run this appliance?\n");
-      double hours = scanner.nextDouble();
-
-      Appliance appliance = new Appliance(volts,amps,hours);
-      appliances.put(name,appliance);
+        Appliance appliance = new Appliance(volts, amps, hours);
+        appliances.put(name, appliance);
+      }
+    }
+    System.out.printf("Appliance            Volts    Amps    Hrs        KWH\n");
+    for (Map.Entry<String, Appliance> entry : appliances.entrySet()) {
+      Appliance appliance = entry.getValue();
+      System.out.printf("%-20s%6.0f%8.2f%8.2f%10.2f%n", entry.getKey(), appliance.voltage,
+          appliance.amps, appliance.hours, appliance.getDeviceKWH());
+    }
+    System.out.println();
+    System.out.printf("Appliance           $Appl     %%ofBill\n");
+    for (Map.Entry<String, Appliance> entry : appliances.entrySet()) {
+      Appliance appliance = entry.getValue();
+      Bill bill = new Bill(appliance);
+      System.out.printf("%-20s$%-6.2f%8.2f%%%n", entry.getKey(), bill.getDeviceMonthlyBill(),
+          bill.getPercentage());
+      System.out.printf("%nYour total monthly bill is approximately: $%1.2f",
+          bill.getMonthlyBill());
     }
 
-   for( Map.Entry<String,Appliance> entry : appliances.entrySet()){
-     Appliance appliance = entry.getValue();
-     System.out.printf("%-20s%6.0f%8.2f%6.2f%10.2f%n", entry.getKey(), appliance.voltage,
-         appliance.amps, appliance.hours, appliance.getDeviceKWH());
-   }
-
-//    Console console = System.console();
-//    String state = console.readLine("Please provide the state abbreviation of your residency:\n"
-//        + "(ex. Enter 'NY' for New York or 'CO' for Colorado)\n ");
-//    System.out.println(state);
-//
-//    Appliance AC = new Appliance(120.0, 37.5, 17.5);//78.75
-//    Appliance heater = new Appliance(120.0, 12.5, 0.0);//0
-//    Appliance tv = new Appliance(120.0, 0.5, 7.0);//0.42
-//    Appliance refrigerator = new Appliance(115.0, 6.04, 8.0);//5.5568
-//    Appliance dishwasher = new Appliance(120.0, 10.5, 3.0);//3.78
-//    Appliance microwave = new Appliance(120, 5.0, 0.5);//0.3
-//    Appliance lighting = new Appliance(120, 15, 5);//9.00
-//
-//    Bill aCbill = new Bill(AC);
-//    Bill tvBill = new Bill(tv);
-//
-//    System.out.printf("The television takes up %1.2f%% of your energy bill.\n",
-//        tvBill.getPercentage());
-//    System.out.printf("The Air Conditioning takes up %1.2f%% of your energy bill.\n",
-//        aCbill.getPercentage());
-//
-//    System.out.printf("Your daily bill for Air Conditioning is $%1.2f\n",
-//        aCbill.getDeviceDailyBill());
-//    System.out.printf("Your monthly bill for Air Conditioning is $%1.2f\n",
-//        aCbill.getDeviceMonthlyBill());
-//    System.out.printf("Your yearly bill for Air Conditioning is $%1.2f\n",
-//        aCbill.getDeviceYearlyBill());
-//
-//    System.out.printf("Your daily bill is $%1.2f\n", aCbill.getDailyBill());
-//    System.out.printf("Your monthly bill is $%1.2f\n", aCbill.getMonthlyBill());
-//    System.out.printf("Your yearly bill is $%1.2f\n", aCbill.getYearlyBill());
   }
-
 }
